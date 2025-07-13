6a98d0102a1b24c9e10045d92bb77387bf475d14
Marykouva <marykouvedou@gmail.com>
2025-07-13 20:06:26 +0000

    First python code

---
+++ FirstCalculator.py
@@ -0,0 +1,17 @@
+#Divide, Multiply & Subtract figures in python
+#Enter first number
+num1 = float(input("Enter first number"))
+#Enter second number
+num2 = float(input("Enter second number"))
+#Add the two numbers
+AddNum = num1 + num2 
+print(f"Sum:{AddNum}")
+#Subtract the numbers
+SubNum = num1 - num2
+print(f"Subtraction:{SubNum}")
+#Multiply the numbers
+MultiplyNum = num1 * num2
+print(f"Multiplication:{MultiplyNum}") 
+#Divide the numbers
+DivideNum = num1 / num2
+print(f"Divide:{DivideNum}")
